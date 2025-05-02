df1d4735e566bbfa27b6f8b5249d35ef49d1c904
Your Name <qliu0048@student.monash.edu>
2025-05-02 08:49:15 +0800

    google login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("org.jetbrains.kotlin.kapt")
-    id("com.google.gms.google-services") // ✅ 不加 version
+    id("com.google.gms.google-services")
 }
 
 android {
