29fb9afc9b60ca0fdf7cd1b134d28e7860c21b2b
jliu0361 <jliu0361@student.monash.edu>
2025-05-18 17:41:51 +1000

    update app version

--- app/src/main/java/com/example/fit5046a3a4/data/AppDatabase.kt
+++ app/src/main/java/com/example/fit5046a3a4/data/AppDatabase.kt
@@ -14,7 +14,7 @@ import androidx.room.RoomDatabase
         FoodCategoryEntity::class,
         FoodItemEntity::class
     ],
-    version = 4,
+    version = 5,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
