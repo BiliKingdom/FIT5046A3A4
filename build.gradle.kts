a37d78bd89018b113376748b49113052c4579be6
TongyiXie <892057620@qq.com>
2025-05-19 15:06:35 +1000

    fix weather api bug

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    kotlin("plugin.serialization") version "1.8.10" apply false
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
